7017951a988d96fdda592fd4ec0a8b9556c638d7
masete <nicholasmasete72@gmail.com>
2022-08-21 17:42:26 +0300

    feat: function to display all navbar ites

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,44 @@ import React from 'react';
 import { FaBars, FaTimes} from 'react-icons/fa'
 
 const NavBar = () => {
+
+    const links = [
+        {
+            id:1,
+            link: 'home',
+        },
+        {
+            id:2,
+            link: 'about',
+        },
+        {
+            id:3,
+            link: 'portfolio',
+        },
+        {
+            id:4,
+            link: 'experience',
+        },
+        {
+            id:5,
+            link: 'contact',
+        }
+    ]
   return (
     <div className='flex justify-between items-center w-full h-20 fixed text-white bg-black p-4'>
         <div>
             <h1 className='text-6xl font-signature ml-4'>masete</h1>
         </div>
         <ul className='flex'>
-            <li className='px-2 text-gray-500 font-medium cursor-pointer capitalize hover:scale-105 duration-200'>
-                Home
-            </li>
-            <li>Home</li>
+
+            {links.map(({id, link}) => (
+                <li 
+                key={id} 
+                className='px-2 text-gray-500 font-medium cursor-pointer capitalize hover:scale-105 duration-200'>
+                {link}
+                </li>
+            ))} 
+            
         </ul>
     </div>
   )
