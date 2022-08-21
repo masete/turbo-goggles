ed1fabe50c73db626579dfce117c63ae8822a5cf
masete <nicholasmasete72@gmail.com>
2022-08-21 16:56:08 +0300

    feat: working on the nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 export default function App() {
   return (
-    <h1 className="text-2xl font-bold underline font-signature">
-      nicholas asete!
-    </h1>
+    <>
+    <div>
+      <NavBar />
+    </div>
+    </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaBars, FaTimes} from 'react-icons/fa'
+
+const NavBar = () => {
+  return (
+    <div className='flex justify-between items-center w-full h-20 fixed text-white bg-black'>
+        <div>
+            <h1>masete</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
