eb95d1cd344e00ebf8ce924a612d2f919ef38a14
masete <nicholasmasete72@gmail.com>
2022-08-21 17:25:13 +0300

    feat: styled the hoe

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,12 @@ const NavBar = () => {
         <div>
             <h1 className='text-6xl font-signature ml-4'>masete</h1>
         </div>
+        <ul className='flex'>
+            <li className='px-2 text-gray-500 font-medium cursor-pointer capitalize hover:scale-105 duration-200'>
+                Home
+            </li>
+            <li>Home</li>
+        </ul>
     </div>
   )
 }
