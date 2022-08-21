5fcc0dde2f3defcb276a70722ee9ebb35f20cf2b
masete <nicholasmasete72@gmail.com>
2022-08-21 17:05:29 +0300

    feat: worked on a custo logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,9 @@ import { FaBars, FaTimes} from 'react-icons/fa'
 
 const NavBar = () => {
   return (
-    <div className='flex justify-between items-center w-full h-20 fixed text-white bg-black'>
+    <div className='flex justify-between items-center w-full h-20 fixed text-white bg-black p-4'>
         <div>
-            <h1>masete</h1>
+            <h1 className='text-6xl font-signature ml-4'>masete</h1>
         </div>
     </div>
   )
