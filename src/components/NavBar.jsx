429e3542e775241e089f8c64f28719f86074bfaa
masete <nicholasmasete72@gmail.com>
2022-08-21 18:19:22 +0300

    feat: added horizontal mobile navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FaBars, FaTimes} from 'react-icons/fa'
+import { useState } from 'react';
+import { FaBars, FaTimes} from 'react-icons/fa';
 
 const NavBar = () => {
+    const [nav, setNav] = useState(false)
 
     const links = [
         {
@@ -41,9 +43,24 @@ const NavBar = () => {
             ))} 
             
         </ul>
-        <div className='cursor-pointer pr-4 z-10 text-gray-500'>
-            <FaBars size={30} />
+        <div 
+        onClick={() => setNav(!nav)}
+        className='cursor-pointer pr-4 z-10 text-gray-500'>
+            {nav ? (<FaBars size={30}/>):(<FaTimes size={30}/>)}
+        
         </div>
+
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+
+            {links.map(({id, link}) => (
+            <li 
+                key={id} 
+                className='px-4 capitalize py-6 cursor-pointer text-4xl'>
+                {link}
+            </li>
+            ))} 
+            
+        </ul>
     </div>
   )
 }
