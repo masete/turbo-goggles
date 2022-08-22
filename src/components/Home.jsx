01af38493c72609ce11120efb10e6797d3da39ea
masete <nicholasmasete72@gmail.com>
2022-08-22 15:32:39 +0300

    feat: added arrow and profile iage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,38 @@ import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 const Home = () => {
   return (
     <>
-    <div>
-      <div>
+    <div name='home' className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+
+      <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+
         <div>
+
           <h2>
               Iam a FullStack Developer
           </h2>
+
           <p>
           I have 8 years of experience building and desgining software.
             Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            React, Tailwind, Django, PostgresQl and Git.
           </p>
+
+          <div>
+            Portfolio
+            <span>
+              <MdOutlineKeyboardArrowRight />
+            </span>
+            
+          </div>
+
         </div>
+
+          <img className='h-[350px] rounded-2xl mx-auto w-2/3 md:w-full' src={HeroImage} alt='profile'/>
+
       </div>
     </div>
       
-      {/* <img src={HeroImage} /> */}
+      
     </>
   )
 }
