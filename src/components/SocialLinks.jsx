27990e95b4034698b8b053db9db011e3efa1164d
masete <nicholasmasete72@gmail.com>
2022-08-22 17:18:16 +0300

    feat: added hover effect on social link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -8,7 +8,7 @@ const SocialLinks = () => {
     <>
     <div className='flex flex-col top-[35%] fixed left-0'>
         <ul>
-            <li className='flex justify-between items-center w-40 h-1/4 px-4'>
+            <li className='flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md bg-gray-500 duration-300'>
                 <a href='' className='flex justify-between text-white items-center w-full'>
                     <> LinkedIn <FaLinkedinIn size={30}/></>
                 </a>
