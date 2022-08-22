ccd8ac8a65cc7f6e90c52b19ebeb29835b9dbc54
masete <nicholasmasete72@gmail.com>
2022-08-22 21:00:02 +0300

    feat: added portfolio page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import SocialLinks from './components/SocialLinks';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <NavBar />
       <Home />
       <About />
+      <Portfolio />
 
 
       <SocialLinks />
