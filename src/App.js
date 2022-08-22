70d59193136250f2237cb6ad4c4e35febd923d64
masete <nicholasmasete72@gmail.com>
2022-08-22 14:50:41 +0300

    feat: worked on hoe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <>
     <div>
       <NavBar />
+      <Home />
     </div>
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import HeroImage from '../assets/heroimage.jpg';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div>
+      <div>
+        <div>
+          <h2>
+              Iam a FullStack Developer
+          </h2>
+          <p>
+          I have 8 years of experience building and desgining software.
+            Currently, I love to work on web application using technologies like
+            React, Tailwind, Next JS and GraphQL.
+          </p>
+        </div>
+      </div>
+    </div>
+      
+      {/* <img src={HeroImage} /> */}
+    </>
   )
 }
 
