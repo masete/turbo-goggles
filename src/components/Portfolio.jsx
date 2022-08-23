9c1d6874c4cdddd22349ac8d3711319704a9882b
masete <nicholasmasete72@gmail.com>
2022-08-23 19:13:48 +0300

    feat: styled p tags

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,10 +11,11 @@ const Portfolio = () => {
     <>
     <div name='portfolio' className="bg-gradient-to-b from-black to-gray-800 w-full text-white md: h-screen">
 
-      <div>
-        <div>
-          <p>Portfolio</p>
-          <p>Checkout Some of my work right here</p>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+          <p className="py-6">Checkout Some of my work right here</p>
         </div>
 
         <div>
