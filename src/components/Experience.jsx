d105fcb63cacd73b00f07c9bffcf982cc386c390
masete <nicholasmasete72@gmail.com>
2022-08-24 19:18:07 +0300

    feat: added styling to p tags

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,10 +18,10 @@ const Experience = () => {
                 <p className='py-6'>These are technologies that i have worked with.</p>
             </div>
 
-            <div>
-                <div>
-                    <img src={html} alt='jkkjk'/>
-                    <p>HTml</p>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                <div className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
+                    <img src={html} alt='jkkjk' className='w-20 mx-auto'/>
+                    <p className='mt-4'>HTml</p>
                 </div>
             </div>
         </div>
