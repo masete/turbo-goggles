dabe4cd302ad25a863f746bf322d073b3bab68ea
masete <nicholasmasete72@gmail.com>
2022-08-24 20:08:36 +0300

    feat: added textarea

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,23 @@ const  Contact = () => {
                 <p>Contact</p>
                 <p>Submit this form to get in touch with me...</p>
             </div>
+
+            <div>
+                <form action=''>
+                    <input type='text' name='name' placeholder='Enter your name here' 
+                    className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+                    />
+
+                    <input type='email' name='name' placeholder='Enter your email here' 
+                    className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+                    />
+
+                    <textarea name='message' rows='10' 
+                    className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'>
+                        
+                    </textarea>
+                </form>
+            </div>
         </div>
     </div>
     </>
