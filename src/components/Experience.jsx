01c9f44e934f57011a8c284c2f62ea75b71d6799
masete <nicholasmasete72@gmail.com>
2022-08-24 19:06:29 +0300

    feat: added styling to p tags

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,16 +11,16 @@ import tailwind from "../assets/tailwind.png";
 const Experience = () => {
   return (
     <>
-    <div>
-        <div>
+    <div name='experience' className='bg-gradient-to-b from-gray-500 to-black w-full h-screen'>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
-                <p>Experience</p>
-                <p>These are technologies that i have worked with.</p>
+                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+                <p className='py-6'>These are technologies that i have worked with.</p>
             </div>
 
             <div>
                 <div>
-                    <img src={html} />
+                    <img src={html} alt='jkkjk'/>
                     <p>HTml</p>
                 </div>
             </div>
