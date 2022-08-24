d63b4820062fe5de36a0020d37b08aad4a700985
masete <nicholasmasete72@gmail.com>
2022-08-24 23:34:42 +0300

    feat: styling contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const  Contact = () => {
   return (
     <>
-    <div>
-        <div>
+    <div className='w-full h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
+        <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
             <div>
                 <p>Contact</p>
                 <p>Submit this form to get in touch with me...</p>
@@ -22,8 +22,11 @@ const  Contact = () => {
 
                     <textarea name='message' rows='10' 
                     className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'>
-                        
+
                     </textarea>
+
+                    <button 
+                    className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Lets Talk</button>
                 </form>
             </div>
         </div>
