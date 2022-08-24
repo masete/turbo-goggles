0690237aac7901c53b9721b1ced49bcdf465a1e1
masete <nicholasmasete72@gmail.com>
2022-08-24 23:44:47 +0300

    feat: styling all the fields on contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,17 +10,17 @@ const  Contact = () => {
                 <p className="py-6">Submit this form to get in touch with me...</p>
             </div>
 
-            <div>
-                <form action=''>
+            <div className='flex justify-center items-center'>
+                <form action='' className='flex flex-col w-full md:w-1/2'>
                     <input type='text' name='name' placeholder='Enter your name here' 
                     className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
                     />
 
                     <input type='email' name='name' placeholder='Enter your email here' 
-                    className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+                    className='p-2 bg-transparent border-2 rounded-md my-4 text-white focus:outline-none' 
                     />
 
-                    <textarea name='message' rows='10' 
+                    <textarea name='message' rows='10' placeholder='Enter your message'
                     className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'>
 
                     </textarea>
